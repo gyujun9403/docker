a4b178bb82431723e614986fb2e2eb1666fe1eef
gyeon <75626815+gyujun9403@users.noreply.github.com>
2022-12-15 19:43:13 +0900

    root 비번 설정 문제 해결?
    DB루트 비번이 지끔까지 설정 안되어 있었음...

--- 42Inception/Doit/srcs/requirements/mariadb/tools/setup.sh
+++ 42Inception/Doit/srcs/requirements/mariadb/tools/setup.sh
@@ -6,9 +6,12 @@ if [ $? -ne 0 ]; then
   mysql -e "CREATE DATABASE IF NOT EXISTS $MARIADB_DB_NAME";
   mysql -e "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PWD'";
   mysql -e "GRANT ALL PRIVILEGES ON $MARIADB_DB_NAME.* TO '$MARIADB_USER'@'%'";
-  mysql -e "UPDATE mysql.user SET Password = PASSWORD('${MARIADB_ROOT_PWD}') WHERE User = 'root'"
-  mysql -e "FLUSH PRIVILEGES;"
-  mysqladmin -u $MARIADB_ROOT -p shutdown
+  mysql -e "set password=password('$MARIADB_ROOT_PWD');"
+  mysql -u$MARIADB_ROOT -p$MARIADB_ROOT_PWD -e "FLUSH PRIVILEGES;"
+  mysqladmin -u$MARIADB_ROOT -p$MARIADB_ROOT_PWD shutdown
+  #mysql -e "UPDATE mysql.user SET Password = PASSWORD('${MARIADB_ROOT_PWD}') WHERE User = 'root'"
+  #mysql -e "FLUSH PRIVILEGES;"
+  #mysqladmin -u $MARIADB_ROOT -p shutdown
   touch /var/lib/mysql/.setup
 fi
 exec mysqld
