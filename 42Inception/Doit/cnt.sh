1771866653c1cc18f8cbdc75bd497606d6b60a78
Gyujun Yeon <gyeon@c5r8s2.42seoul.kr>
2022-12-16 18:16:04 +0900

    fin2.1

--- 42Inception/Doit/cnt.sh
+++ 42Inception/Doit/cnt.sh
@@ -1,5 +1,5 @@
 echo -ne "\033[0;33mWaiting"
-for i in {11..01}
+for i in {12..01}
 do
 echo -en "."
 sleep 1
--- 42Inception/Doit/srcs/requirements/mariadb/tools/setup.sh
+++ 42Inception/Doit/srcs/requirements/mariadb/tools/setup.sh
@@ -7,6 +7,7 @@ if [ $? -ne 0 ]; then
   mysql -e "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PWD'";
   mysql -e "GRANT ALL PRIVILEGES ON $MARIADB_DB_NAME.* TO '$MARIADB_USER'@'%'";
   mysql -e "set password=password('$MARIADB_ROOT_PWD');"
+  sleep 1
   mysql -u$MARIADB_ROOT -p$MARIADB_ROOT_PWD -e "FLUSH PRIVILEGES;"
   mysqladmin -u$MARIADB_ROOT -p$MARIADB_ROOT_PWD shutdown
   touch /var/lib/mysql/.setup
